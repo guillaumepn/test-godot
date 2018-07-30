018b894872883f1d4a32495d998cc9c56e3ba4c3
guillaumepn <guillaumepn@free.fr>
2018-07-30 21:27:01 +0200

    smoother movements + capsule shape

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,31 +2,40 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 200
+const ACCELERATION = 30
+const MAXSPEED = 200
 const JUMP_HEIGHT = -550
 
 var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += GRAVITY
+	var friction = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACCELERATION, MAXSPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x - ACCELERATION, -MAXSPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("run")
 	else:
-		motion.x = 0
 		$Sprite.play("idle")
+		friction = true
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
-		$Sprite.play("jump")
+		if motion.y < 0:
+			$Sprite.play("jump")
+		else:
+			$Sprite.play("fall")
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.05)
 			
 	motion = move_and_slide(motion, UP)
 	
